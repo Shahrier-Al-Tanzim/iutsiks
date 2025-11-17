3c49edc408b1202013aca9a9fe26c6af49a75de3
Ryuzaki-L-07 <shahrier@iut-dhaka.edu>
2025-11-17 10:40:13 +0600

    Authentication removed from dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,12 @@ use App\Http\Controllers\EventController;
 
 use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
